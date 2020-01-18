0bcb4a792e64bc8dac65e0903e4f06ec17a7ed33
KinNeko <nils@diekmann-germany.de>
2020-01-18 17:01:42 +0100

    - added example files for import vs import public; - added unit test to show the difference in the model;

--- Protobuf2UmlLibraryTest/UnitTest1.cs
+++ Protobuf2UmlLibraryTest/UnitTest1.cs
@@ -46,5 +46,36 @@ namespace Protobuf2UmlLibraryTest
 				}
 			}
 		}
+
+		[Test]
+		public void ExampleImport()
+		{
+			using (var stream = File.OpenRead(@"protobuf/example.import"))
+			{
+				FileDescriptorSet descriptorSet = FileDescriptorSet.Parser.ParseFrom(stream);
+				Google.Protobuf.Collections.RepeatedField<FileDescriptorProto> descriptorsProto = descriptorSet.File;
+				FileDescriptorProto a = descriptorsProto[0];
+				FileDescriptorProto b = descriptorsProto[1];
+				FileDescriptorProto bb = descriptorsProto[2];
+				FileDescriptorProto c = descriptorsProto[3];
+				FileDescriptorProto cc = descriptorsProto[4];
+
+				Assert.That(b.Dependency.Count, Is.EqualTo(1));
+				Assert.That(b.Dependency[0], Contains.Substring("A.proto"));
+				Assert.That(b.PublicDependency.Count, Is.EqualTo(0));
+				
+				Assert.That(bb.Dependency.Count, Is.EqualTo(1));
+				Assert.That(bb.Dependency[0], Contains.Substring("B.proto"));
+				Assert.That(bb.PublicDependency.Count, Is.EqualTo(0));
+
+				Assert.That(c.Dependency.Count, Is.EqualTo(1));
+				Assert.That(c.Dependency[0], Contains.Substring("A.proto"));
+				Assert.That(c.PublicDependency.Count, Is.EqualTo(1));
+
+				Assert.That(cc.Dependency.Count, Is.EqualTo(1));
+				Assert.That(cc.Dependency[0], Contains.Substring("C.proto"));
+				Assert.That(cc.PublicDependency.Count, Is.EqualTo(1));
+			}
+		}
 	}
 }
---
+++ Protobuf2UmlLibraryTest/protobuf/protoc.example.import.bat
@@ -0,0 +1,2 @@
+protoc "./example/import/**" --proto_path="." --descriptor_set_out="example.import"
+pause
